bacd8d40aff011ee1050307613b97d94d3a6db1c
programmershakib <programmershakibinfo@gmail.com>
2024-10-21 18:15:06 +0600

    first commit

---
+++ src/components/Bottles/Bottles.jsx
@@ -0,0 +1,73 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Bottle from "../Bottle/Bottle";
+import './Bottles.css'
+import { addToLS, getStoredCart, removeFromLS } from "../../utilities/localStorage";
+import Cart from "../Cart/Cart";
+
+const Bottles = () => {
+    const [bottles, setBottles] = useState([]);
+    const [cart , setCart] = useState([]);
+
+    useEffect(() => {
+        fetch('bottles.json')
+        .then(res => res.json())
+        .then(data => setBottles(data))
+    },[])
+
+    // load cart from local storage
+    useEffect(() => {
+        console.log('called the useEffect', bottles.length);
+        if(bottles.length > 0){
+            const storedCart = getStoredCart();
+            console.log(storedCart, bottles);
+
+            const saveCart = [];
+
+            for(const id of storedCart){
+                console.log(id);
+                const bottle = bottles.find(bottle => bottle.id === id);
+                if(bottle){
+                    saveCart.push(bottle);
+                }
+            }
+            console.log('saved cart', saveCart);
+            setCart(saveCart);
+
+        }
+    },[bottles])
+
+    const handleAddToCart = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+        addToLS(bottle.id);
+    }
+
+    const handleRemoveFromCart = id => {
+        // visual cart remove
+        const remainingCart = cart.filter(bottle=> bottle.id !==id);
+        setCart(remainingCart);
+        // remove from LS
+        removeFromLS(id);
+    }
+
+    return (
+        <div>
+            <h2>Bottles Available: {bottles.length}</h2>
+            <Cart handleRemoveFromCart={handleRemoveFromCart} cart={cart}></Cart>
+
+
+            <div className="bottle-container">
+                {
+                    bottles.map(bottle => <Bottle 
+                        key={bottle.id} 
+                        bottle={bottle}
+                        handleAddToCart={handleAddToCart}
+                        ></Bottle>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Bottles;
